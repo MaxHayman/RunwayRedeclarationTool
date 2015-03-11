c583ce593e2aeef9aa3a52597a0ae708dca63f54
Max Hayman <maxhayman@maxhayman.co.uk>
2015-03-11 13:19:30 +0000

    *ops

--- src/DisplayMenuBar.java
+++ src/DisplayMenuBar.java
@@ -7,23 +7,12 @@ import javax.swing.JMenuItem;
 
 public class DisplayMenuBar extends JMenuBar {
 
-	/*
-	 * panel - is the FractalPanel the Menu is controlling.
-	 */
 	private Frame2D frame;
 	
-	/*
-	 * Constructor for FractalMenuBar which takes
-	 * a FractalPanel which it will be controlling
-	 * in its parameters.
-	 */
 	public DisplayMenuBar(Frame2D frame)
 	{
 		this.frame = frame;
-		
-		/*
-		 * Variables used in the creation of the menu bar.
-		 */
+
 		JMenu menu;
 		JMenuItem menuItem;
 
@@ -49,33 +38,16 @@ public class DisplayMenuBar extends JMenuBar {
 		menuItem.addActionListener(new SaveListener("GIF"));
 		saveMenuItem.add(menuItem);
 	}
-	
-	/*
-	 * ColourListener will listen for when a user clicks one
-	 * of the save file formats.
-	 */
+
 	public class SaveListener implements ActionListener
 	{
-		/*
-		 * format - stores the file format a particular button
-		 *          represents. 
-		 */
 		private String format;
 		
-		/*
-		 * Constructor for SaveListener which takes the file format
-		 * in its parameters.
-		 */
 		public SaveListener(String format)
 		{
 			this.format = format;
 		}
 
-		/*
-		 * Inherited from ActionListener. It will trigger when a user
-		 * clicks a file format to save and will tell the fractal to save
-		 * itself to the particular file format selected.
-		 */
 		public void actionPerformed(ActionEvent e) {
 			frame.display.saveFractal(format);
 		}
